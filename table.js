6ae60aa13a5a738b7583be6111d495824471197e
Mikita Manko <mikita_manko@epam.com>
2014-02-20 00:49:36 -0800

    refactored

--- table.js
+++ table.js
@@ -1,21 +1,16 @@
 window.responsiveTable = (function (){
-    var responsiveRules = 
-        'table, thead, tbody, th, td, tr {display: block;}' + 
-        'thead tr {position: absolute;top: -9999px;left: -9999px;}' +
-        'tr {border: 1px solid #ccc;}' + 
-        'tr td {border: none; position: relative; padding-left: 50%;}' +
-        'tr td:before {position: absolute; left: 6px; width: 45%; white-space: nowrap;}';
 
-	function addResponsiveRule(breakPoint, selector, rules){
-        var rule = '@media (max-width : ' + breakPoint + ') { ' + 
-            selector + ' { ' + rules + '; } }'
-		document.styleSheets[0].insertRule(rule, 0);
-	}
+	function itemized (table, breakPoint){
+        var headers = table.querySelectorAll('th');
+        var responsiveRules = 
+            'table, thead, tbody, th, td, tr {display: block;}' + 
+            'thead tr {position: absolute;top: -9999px;left: -9999px;}' +
+            'tr {border: 1px solid #ccc;}' + 
+            'tr td {border: none; position: relative; padding-left: 50%;}' +
+            'tr td:before {position: absolute; width: 45%; white-space: nowrap;}';
+        
+        breakPoint = breakPoint || '870px';
 
-	return function (table, breakPoint){
-		breakPoint = breakPoint || '870px';
-
-		var headers = table.querySelectorAll('.responsive-table th');
 		for(var i = 0, len = headers.length; i < len; i++) {
 			responsiveRules += 'td:nth-of-type(' + (i + 1) + '):before ' + 
 				' { content: "' + headers[i].innerHTML + '" }';
@@ -24,4 +19,7 @@ window.responsiveTable = (function (){
 		document.styleSheets[0].insertRule('@media (max-width : ' + 
 			breakPoint + ') { ' + responsiveRules + ' }', 0);
 	};
+    return {
+        itemized: itemized
+    };
 })();
